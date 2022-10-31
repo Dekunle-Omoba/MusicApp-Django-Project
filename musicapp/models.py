e957536fc921a1d81ecf613512eaa4565ee0e637
Your Name <boldlionk@gmail.com>
2022-10-31 21:52:31 +0100

    Second Commit

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,4 +1,4 @@
-from tkinter import CASCADE
+
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
@@ -7,17 +7,19 @@ from django.db import models
 
 class Artiste(models.Model):
     first_name = models.TextField
-    last_name = models.CharField(_MAX_LENGTH-50)
-    age = models.IntegerField(_MAX_LENGTH-10)
+    last_name = models.TextField
+    age = models.IntegerField(_MAX_LENGTH - 10)
 
 
 class Song(models.Model):
- song_title = models.CharField(_MAX_LENGTH-50)
- date_released = models.DateField(_MAX_LENGTH-25)
- likes = models.IntegerField(_MAX_LENGTH-25)
- Artiste_id = models.IntegerField(_MAX_LENGTH-25)
+    Artiste = models.ForeignKey(Artiste,on_delete=models.CASCADE)
+    song_title = models.TextField
+    date_released = models.DateField(_MAX_LENGTH - 25)
+    likes = models.IntegerField(_MAX_LENGTH - 25)
+    artiste_id = models.IntegerField(_MAX_LENGTH - 25)
 
 
 class Lyric(models.Model):
- content  = models.CharField(_MAX_LENGTH-300)
- song_id = models.IntegerField(_MAX_LENGTH-25)
+    Song = models.ForeignKey(Song,on_delete=models.CASCADE)
+    content = models.TextField
+    song_id = models.IntegerField(_MAX_LENGTH - 10)
